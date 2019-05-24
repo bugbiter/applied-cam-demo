2859d4f9f94254fcca1ee7e161fa3c5808aef9d9
haakonsbakken <haakonsbakken@iot-labs.no>
2019-05-24 15:10:22 +0200

    add setup & license, improved pwm test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # applied-cam-demo
 Demo for camera tilt & pan using Google PubSub for angle input
+
+#virutalenv ..
+#sudo apt-get install -y python-pip
+#sudo pip install wiringpi
+#pip install google-cloud-pubsub
--- servotest.py
+++ servotest.py
@@ -1,6 +1,3 @@
-#sudo apt-get install -y python-pip
-#sudo pip install wiringpi
-
 # Servo Control
 import time
 import wiringpi
@@ -16,6 +13,7 @@ import wiringpi
 wiringpi.wiringPiSetupGpio()
  
 # set #18 to be a PWM output
+wiringpi.pinMode(13, wiringpi.GPIO.PWM_OUTPUT)
 wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
  
 # set the PWM mode to milliseconds stype
@@ -28,9 +26,12 @@ wiringpi.pwmSetRange(2000)
 delay_period = 0.01 #0.1 .. 0.001, slower .. faster
  
 while True:
-  for pulse in range(55, 252, 1): #50 (0.5ms) to 250 (2.5ms)
-    wiringpi.pwmWrite(18, pulse)
+  #55 (0.55ms) to 252 (2.52ms)
+  for (p, p2) in [(p, p2) for p in range(55, 252, 1) for p2 in range(252, 55, -1)]:
+    wiringpi.pwmWrite(13, p)
+    wiringpi.pwmWrite(18, p2)
     time.sleep(delay_period)
-  for pulse in range(252, 55, -1):
-    wiringpi.pwmWrite(18, pulse)
+  for (p, p2) in [(p, p2) for p in range(252, 55, -1) for p2 in range(55, 252, 1)]:
+    wiringpi.pwmWrite(13, p)
+    wiringpi.pwmWrite(18, p2)
     time.sleep(delay_period)
